c488e0a0e479e729f5ed895014e6c3e222948966
Maxim <maxim.poliakov@stud.tu-darmstadt.de>
2024-03-25 02:51:30 +0100

    main loop is more efficient now

--- src/main/java/org/example/CameraAccess.java
+++ src/main/java/org/example/CameraAccess.java
@@ -1,43 +1,35 @@
 package org.example;
-import org.example.MyMat;
-import org.example.TelegramBot;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.VideoWriter;
 import org.opencv.videoio.Videoio;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class CameraAccess {
 
-
-
     static {
         try {
-            // Directly load the OpenCV native library
-            System.load(System.getProperty("user.dir") + "\\src\\main\\resources\\opencv_java490.dll"); // Replace with the actual path
+            System.load(System.getProperty("user.dir") + "\\src\\main\\resources\\opencv_java490.dll");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Full Screen Message");
-        createJFrame(frame);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        JFrame frame = createJFrame();
 
         TelegramBot bot = new TelegramBot();
         try {
@@ -47,64 +39,81 @@ public class CameraAccess {
             e.printStackTrace();
         }
 
-        VideoCapture camera = new VideoCapture(0); // 0 for default camera
+        VideoCapture camera = new VideoCapture(0); //0 for default camera
 
         if (!camera.isOpened()) {
             System.out.println("Error: Camera not accessible");
             return;
         }
 
+
+        System.out.println("Camera name: " + camera.getBackendName());
+        System.out.println("Camera FPS: " + camera.get(Videoio.CAP_PROP_FPS));
+        System.out.println("Camera res: " + camera.get(Videoio.CAP_PROP_FRAME_HEIGHT) + " * " + camera.get(Videoio.CAP_PROP_FRAME_WIDTH));
+
         Mat currentFrame = new Mat();
         Mat prevFrame = new Mat();
+        String filename = "src/main/resources/output.mp4";
+        int fourcc = VideoWriter.fourcc('X', '2', '6', '4'); //X264 codec
+        double frameRate = 30;
+        Size frameSize = new Size(camera.get(Videoio.CAP_PROP_FRAME_WIDTH), camera.get(Videoio.CAP_PROP_FRAME_HEIGHT));
+        VideoWriter writer = new VideoWriter(filename, fourcc, frameRate, frameSize, true);
 
-        System.out.println(camera.get(Videoio.CAP_PROP_FPS));
+        boolean motionDetected = false;
+        Color color = Color.BLUE;
 
-        System.out.println(camera.getBackendName());
-        long startTime = System.currentTimeMillis();
-        long prevTime1 = startTime;
-        long prevTime2 = startTime;
-        boolean colorToggle = false;
-        boolean lockPC = false;
+        int loopCounter = 0;
 
+        //less equals 30 iterations per second
         while (true) {
             if (camera.read(currentFrame)) {
-                System.out.println(Arrays.toString(currentFrame.get(200, 200)));
 
                 HighGui.imshow("Camera Feed", currentFrame);
                 if (HighGui.waitKey(10) == 27) { // Exit on 'ESC'
                     break;
                 }
-                Mat blurredCurrentFrame = new Mat();
-                Imgproc.GaussianBlur(currentFrame, blurredCurrentFrame, new Size(81, 81), 0);
 
                 if (bot.isSystemReady()) {
-                    if (!prevFrame.empty() && !MyMat.compare(blurredCurrentFrame, prevFrame)) {
-                        System.out.println("changed");
-                        if (System.currentTimeMillis() - prevTime1 > 100) {
-                            prevTime1 = System.currentTimeMillis();
-                            Color color = colorToggle ? Color.BLUE : Color.RED;
-                            frame.getContentPane().setBackground(color);
-                            colorToggle = !colorToggle;
-                        }
-                        frame.setVisible(true);
-                        if (System.currentTimeMillis() - prevTime2 > 5000) {
-                            prevTime2 = System.currentTimeMillis();
-                            MyMat.store(currentFrame);
-                            bot.sendImageUploadingAFile("src/main/resources/outputImage.png");
-                        }
-                        lockPC = true;
-                    }
-                    if (lockPC && System.currentTimeMillis() - startTime > 10000)  {
-                        try {
-                            Runtime.getRuntime().exec("rundll32 user32.dll,LockWorkStation");
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                    Mat blurredCurrentFrame = new Mat();
+                    Imgproc.GaussianBlur(currentFrame, blurredCurrentFrame, new Size(81, 81), 0);
+
+                    if (!motionDetected && !prevFrame.empty() && !MyMat.compare(blurredCurrentFrame, prevFrame)) {
+                        motionDetected = true;
+
+                        if (!frame.isVisible()) {
+                            frame.setVisible(true);
                         }
                     }
+
+                    if (motionDetected) {
+                        color = color.equals(Color.BLUE) ? Color.RED : Color.BLUE;
+                        frame.getContentPane().setBackground(color);
+
+                        writer.write(currentFrame);
+
+                        if (loopCounter % 90 == 0) {
+                            executorService.submit(() -> bot.sendImage(currentFrame));
+                        }
+                        if (loopCounter % 300 == 0) {
+                            executorService.submit(() -> {
+                                writer.release();
+                                bot.sendVideo();
+                                /*ProcessBuilder builder = new ProcessBuilder("rundll32", "user32.dll,LockWorkStation");
+                                builder.inheritIO();
+                                try {
+                                    System.out.println("cmd");
+                                    builder.start();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }*/
+                            });
+                        }
+                    }
+                    prevFrame = blurredCurrentFrame.clone();
+                    loopCounter++;
                 } else {
                     frame.setVisible(false);
                 }
-                prevFrame = blurredCurrentFrame.clone();
             } else {
                 throw new RuntimeException("Camera is not accessible!");
             }
@@ -112,26 +121,28 @@ public class CameraAccess {
 
         camera.release();
         HighGui.destroyAllWindows();
+        executorService.shutdown();
+
     }
 
-    private static void createJFrame(JFrame frame) {
+    private static JFrame createJFrame() {
+        JFrame frame = new JFrame("Full Screen Message");
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        // Set full-screen
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
 
-        // Create a label with your message
         JLabel label = new JLabel("STOP", SwingConstants.CENTER);
         label.setFont(new Font("Serif", Font.BOLD, 800));
         label.setForeground(Color.WHITE);
 
-        // Add label to frame
         frame.add(label);
 
-        // Set the frame background color
         frame.getContentPane().setBackground(Color.RED);
+
+        return frame;
     }
 }
 
